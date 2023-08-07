a1a5f159a0cc4269dcf6dc05a233ffb538aaa601
Song014 <ghgsb6200@gmail.com>
2023-08-07 16:16:56 +0900

    security 수정본

--- src/main/java/com/kosa/controller/UserController.java
+++ src/main/java/com/kosa/controller/UserController.java
@@ -5,6 +5,9 @@ import java.util.Map;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +16,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.kosa.dao.UserDAO;
 import com.kosa.service.ShaEncoder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/user")
@@ -56,10 +62,15 @@ public class UserController {
         return "user/login";
     }
 
-    @PostMapping("/logout.do")
-    public String logout() {
-
-        return "redirect:/login";
+    @GetMapping("/logout.do")
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            System.out.println("asd");
+        if (auth != null) {
+            System.out.println("asd2");
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout=true";
     }
 
     @GetMapping("/")
